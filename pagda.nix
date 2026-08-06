154d40e56353a8adfc8db31cfd4af38630241959
whatisRT <andre.knispel@iohk.io>
2026-08-06 18:38:55 +0200

    Update `pagda` & cleanup `pagda.nix`
    Also add .gitignore

--- pagda.nix
+++ pagda.nix
@@ -2,19 +2,7 @@
 
 {
   docs = pagda.docBackends.enhancedHtml {
-    modules = [
-      "CategoricalCrypto"
-      "Categories.Discrete"
-      "Categories.FreeMonoidal"
-      "Categories.FreeStrictMonoidal"
-      "Categories.GradedKleisli"
-      "Categories.MonoidalCoherence"
-      "Categories.NaturalTransformationHelper"
-      "Categories.Properties"
-      "Class.Monad.Ext"
-    ];
     entryModule = "CategoricalCrypto";
     githubUrl = "https://github.com/input-output-hk/categorical-crypto";
-    backButtonUrl = "index.html";
   };
 }
